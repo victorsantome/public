53797ca57256c4b608aac4fd9ad47197601b3d40
victorsantome <74457780+victorsantome@users.noreply.github.com>
2023-02-22 19:25:40 +0100

    Deleted

--- Functions/parameters.py
+++
@@ -1,9 +0,0 @@
-#!python3.8
-
-items : dict = {
-	"mine" : "https://memstest.michelinearthmover.com/suptest/",
-	"auth" : "auth/token",
-	"api" : "wser/",
-	"user" : "VMSapi",
-	"pass" : "vmsapiapi00"
-}
